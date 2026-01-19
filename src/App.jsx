17df948bf182535d0a668f44bf6dc949ef707324
santhoshgodwin <chalz1998@gmail.com>
2026-01-19 20:38:56 +0530

    build issue fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,11 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Users from "./pages/Users";
-import Dashboard from "./pages/Dashboard"
 import NavBar from "./components/NavBar"
 import ApiDemo from "./pages/ApiDemo"
 import Home from "./pages/Home"
 
 function App() {
-  // const [count, setCount] = useState(0)
-
-  // return (
-  //   <>
-  //     <div>
-  //       <a href="https://vite.dev" target="_blank">
-  //         <img src={viteLogo} className="logo" alt="Vite logo" />
-  //       </a>
-  //       <a href="https://react.dev" target="_blank">
-  //         <img src={reactLogo} className="logo react" alt="React logo" />
-  //       </a>
-  //     </div>
-  //     <h1>Vite + React</h1>
-  //     <div className="card">
-  //       <button onClick={() => setCount((count) => count + 1)}>
-  //         count is {count}
-  //       </button>
-  //       <p>
-  //         Edit <code>src/App.jsx</code> and save to test HMR
-  //       </p>
-  //     </div>
-  //     <p className="read-the-docs">
-  //       Click on the Vite and React logos to learn more
-  //     </p>
-  //   </>
-  // )
-
-  // return(
-  //   <BrowserRouter>
-  //     {/* <nav>
-  //       <Link to="/">Users</Link>
-  //     </nav> */}
-
-  //     <Routes>
-  //       <Route path="/" element={<Users/>}/>
-  //     </Routes>
-
-  //   </BrowserRouter>
-  // );
-
-  // return <Dashboard/>;
-
   return (
     <>
       <NavBar/>
